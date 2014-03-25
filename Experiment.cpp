9b4597c28991b505f59c9533534842efe31b13cf
James Palawaga <vsfmag@gmail.com>
2014-03-25 13:41:41 -0600

    SQL Implementation complete & clear database

--- Experiment.cpp
+++ Experiment.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <QtGui>
 #include <QSqlQuery>
 #include <QSqlError>
@@ -12,8 +13,18 @@ Experiment::Experiment(FittsWidget * fw, QSqlDatabase database) {
     bool ok;
     bool queryStatus = false;
     name = QInputDialog::getText(0, "Name", "Please enter your name:", QLineEdit::Normal, "", &ok);
-    query = QSqlQuery(db);
-
+    if (db.open()) {
+        QSqlQuery query(db);
+        if (volunteerId == 0) {
+            query.prepare("INSERT INTO volunteers (name) VALUES(:name)");
+            query.bindValue(":name", name);
+            query.exec();
+            volunteerId = query.lastInsertId().toInt();
+            query.clear();
+        }
+    } else {
+        QMessageBox::warning(this, tr("Done"), tr("DB not open."));
+    }
 }
 
 void Experiment::run() {
@@ -22,36 +33,6 @@ void Experiment::run() {
         experimentNumber++;
     } else {
         QMessageBox::information(this, tr("Done"), tr("Testing is complete."));
-        insertToDatabase();
-    }
-}
-
-void Experiment::insertToDatabase() {
-    for (int j = 0; j < tests.size(); j++) {
-        for (int k = 0; k < tests[j].clicks.size(); k++) {
-            QSqlQuery query(db);
-            if (db.open()) {
-                if (volunteerId == 0) {
-                    query.prepare("INSERT INTO volunteers (name) VALUES(:name)");
-                    query.bindValue(":name", name);
-                    query.exec();
-                    volunteerId = query.lastInsertId().toInt();
-                }
-                query.prepare("INSERT INTO experiments(volunteer_id, testno, size, distance, mouse_x, mouse_y, hit, time) VALUES(:id, :testno, :s, :d, :x, :y, :hit, :time)");
-                query.bindValue(":id", volunteerId);
-                query.bindValue(":testno", j);
-                query.bindValue(":s", tests[j].getSettings().getSize());
-                query.bindValue(":d", tests[j].getSettings().getDistance());
-                query.bindValue(":x", tests[j].clicks[k].point.x());
-                query.bindValue(":y", tests[j].clicks[k].point.y());
-                query.bindValue(":hit", tests[j].clicks[k].hit);
-                query.bindValue(":time", tests[j].clicks[k].time);
-                query.exec();
-                qDebug() << query.lastQuery();
-            } else {
-                QMessageBox::warning(this, tr("Done"), tr("DB not open."));
-            } 
-        }
     }
 }
 
@@ -62,5 +43,14 @@ void Experiment::addExperiment(ExperimentSettings s) {
 
 void Experiment::registerClick(QPoint p, int t, bool h) {
     tests[experimentNumber-1].insertClick(p, t, h);
+    if (db.open()) {
+        QSqlQuery query(db);
+        QString q = QString("INSERT INTO experiments(volunteer_id, testno, size, distance, mouse_x, mouse_y, hit, time) VALUES(%1, %2, %3, %4, %5, %6, %7, %8)").arg(volunteerId).arg(experimentNumber-1).arg(tests[experimentNumber-1].getSettings().getSize()).arg( tests[experimentNumber-1].getSettings().getDistance()).arg(p.x()).arg(p.y()).arg(h).arg(t);
+        query.exec(q);
+        // @TODO Proper error handling
+        //qDebug() << q;
+        //QSqlError e = query.lastError();
+        //qDebug() << e.text();
+        query.clear();
+    }
 }
-
